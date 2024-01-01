04aaf9553969f7fbc009061389059b27c66147fb
SHANTANU GHANWAT <ghanwatshantanu2209@gmail.com>
2024-01-01 14:09:07 +0530

    Add files via upload

---
+++ exam/DSAExam/src/Q2/TestTree.java
@@ -0,0 +1,24 @@
+package Q2;
+
+public class TestTree {
+	public static void main(String[] args) {
+		BinarySearchTree bst = new BinarySearchTree();
+		
+		
+		//int [] arr= {45,12,34,10,51,8,52,59,50};
+		
+		
+		bst.insert(45);
+		bst.insert(12);
+		bst.insert(34);
+		bst.insert(10);
+		bst.insert(51);
+		bst.insert(8);
+		bst.insert(52);
+		bst.insert(59);
+		bst.insert(50);
+		
+		bst.preorder();
+//		System.out.println(bst.toString());
+	}
+}
